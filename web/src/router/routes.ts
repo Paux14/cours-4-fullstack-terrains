ff30054152e3245ad12092e4eaf3142046971cc5
Thibault Barolat-Massole <thibault.barolat@outlook.fr>
2026-01-16 12:43:01 +0100

    routes

---
+++ web/src/router/routes.ts
@@ -0,0 +1,19 @@
+import type { RouteRecordRaw } from 'vue-router'
+
+export const routes: RouteRecordRaw[] = [
+  {
+    name: 'home',
+    path: '/',
+    component: () => import('@/pages/Home.vue'),
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: () => import('@/pages/Register.vue'),
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/pages/Login.vue'),
+  },
+]
